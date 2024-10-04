1aa408696a1c72dc9cea01dd223a240b82ae66d7
Cypher Pepe <125112044+cypherpepe@users.noreply.github.com>
2024-10-04 17:26:59 +0300

    rename bits256 to b256

--- apps/docs-snippets/src/guide/types/b256.test.ts
+++ apps/docs-snippets/src/guide/types/b256.test.ts
@@ -6,13 +6,13 @@ import { Address, arrayify, getRandomB256, hexlify } from 'fuels';
  */
 describe('B256 Types', () => {
   it('should successfully generate and validate bit256 hexed string', () => {
-    // #region bits256-1
+    // #region b256-1
     // #import { getRandomB256 };
 
     // randomB256 is a hexlified string representing a 256-bit value
     const randomB256: string = getRandomB256();
     // 0xbebd3baab326f895289ecbd4210cf886ce41952316441ae4cac35f00f0e882a6
-    // #endregion bits256-1
+    // #endregion b256-1
 
     // #region addresses-2
     const bits256 = '0xbebd3baab326f895289ecbd4210cf886ce41952316441ae4cac35f00f0e882a6';
@@ -23,7 +23,7 @@ describe('B256 Types', () => {
   });
 
   it('should successfully convert between hexed b256 and Uint8Array', () => {
-    // #region bits256-2
+    // #region b256-2
     // #import { arrayify, hexlify, getRandomB256 };
 
     const randomB256: string = getRandomB256();
@@ -33,14 +33,14 @@ describe('B256 Types', () => {
 
     // convert back to hexlified string
     const hexedB256 = hexlify(uint8Arr);
-    // #endregion bits256-2
+    // #endregion b256-2
 
     expect(uint8Arr.length).toBe(32);
     expect(hexedB256).toEqual(randomB256);
   });
 
   it('should successfully generate an address from a b256 string', () => {
-    // #region bits256-3
+    // #region b256-3
     // #import { Address, getRandomB256 };
 
     const randomB256: string = getRandomB256();
@@ -48,6 +48,6 @@ describe('B256 Types', () => {
     const address = Address.fromB256(randomB256);
 
     expect(address.toB256()).toEqual(randomB256);
-    // #endregion bits256-3
+    // #endregion bi256-3
   });
 });
